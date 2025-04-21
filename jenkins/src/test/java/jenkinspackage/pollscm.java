4a0417d3779199961830f54de2a1dda7e3a9e572
kartik <kartik06092001@gmail.com>
2025-04-21 10:54:13 +0530

    pollscm commit

---
+++ jenkins/src/test/java/jenkinspackage/pollscm.java
@@ -0,0 +1,14 @@
+package jenkinspackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pollscm {
+@Test
+public void test() {
+	WebDriver driver=new ChromeDriver();
+	Reporter.log("pollscm welcome",true);
+}
+}
